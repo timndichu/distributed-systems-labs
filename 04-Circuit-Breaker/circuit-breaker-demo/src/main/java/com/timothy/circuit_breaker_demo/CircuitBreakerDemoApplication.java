f09cfc796f288fd784be32ad16311c5fd646f4cb
timndichu <timndichu@gmail.com>
2026-09-22 10:32:02 +0200

    chore: initial spring boot app setup

---
+++ 04-Circuit-Breaker/circuit-breaker-demo/src/main/java/com/timothy/circuit_breaker_demo/CircuitBreakerDemoApplication.java
@@ -0,0 +1,13 @@
+package com.timothy.circuit_breaker_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircuitBreakerDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircuitBreakerDemoApplication.class, args);
+	}
+
+}
---
+++ 04-Circuit-Breaker/circuit-breaker-demo/src/test/java/com/timothy/circuit_breaker_demo/CircuitBreakerDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timothy.circuit_breaker_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircuitBreakerDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
