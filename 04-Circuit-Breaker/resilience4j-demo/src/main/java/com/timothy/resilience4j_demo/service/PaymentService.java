4873c22af1438505935bbe13d4f71bd7fac2784c
timndichu <timndichu@gmail.com>
2026-09-23 16:21:10 +0200

    feat: added v2 of the circuit breaker -adding delay to payment service

--- 04-Circuit-Breaker/resilience4j-demo/src/main/java/com/timothy/resilience4j_demo/service/PaymentService.java
+++ 04-Circuit-Breaker/resilience4j-demo/src/main/java/com/timothy/resilience4j_demo/service/PaymentService.java
@@ -1,5 +1,6 @@
 package com.timothy.resilience4j_demo.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -7,9 +8,26 @@ public class PaymentService {
 
     private boolean available = true;
 
+    @Value("${payment.delay:0}")
+    private long delay;
+
     public String processPayment() {
 
         System.out.println("[PaymentService] processPayment() CALLED");
+
+        // added for V2 - Testing delay in processing time
+         if (delay > 0) {
+            System.out.println(
+                    "[PaymentService] Delaying for " + delay + " ms");
+
+            try {
+                Thread.sleep(delay);
+            } catch (InterruptedException e) {
+                Thread.currentThread().interrupt();
+                throw new RuntimeException(
+                        "Payment processing interrupted", e);
+            }
+        }
 
         if (!available) {
             System.out.println("[PaymentService] FAILING");
