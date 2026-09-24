369426733a0d2e68f5ceb108eb48fc2a4c43155f
timndichu <timndichu@gmail.com>
2026-09-24 16:00:43 +0200

    feat: Load balancer created

---
+++ 07-Load-Balancer/load_balancer/src/test/java/com/timothy/load_balancer/LoadBalancerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timothy.load_balancer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoadBalancerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
